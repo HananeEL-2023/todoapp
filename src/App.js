70ed015bba5d35b2aef14d4cea980ff6b2533b2d
Hanane <hanane.elkaaba1@gmail.com>
2023-11-01 16:27:11 +0100

    Add task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      setTask(e.target.value);
+      setTaskList([...taskList, { name: task, isChecked: false }]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        onKeyDown={handleEnter}
+      />
+      {taskList.map((task, index) => (
+        <div key={index}>
+          {task.isChecked} - {task.name}
+        </div>
+      ))}
+    </>
   );
 }
-
-export default App;
